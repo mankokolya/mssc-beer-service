0929c1a49a32e871deae044a1d1e6dbf85f7b62a
Mykola Manko <mankokolya@gmail.com>
2022-06-12 12:52:41 +0300

    add tests for beerController

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,7 +1,6 @@
 package guru.springframework.msscbeerservice.web.controller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
